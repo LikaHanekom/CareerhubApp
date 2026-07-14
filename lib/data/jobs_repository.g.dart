1a31e221c2713c428fb2f3158b525213efaa45a6
Alika Hanekom <alikahanekom@gmail.com>
2026-07-14 14:13:47 +0200

    Add: Code generation and the new notifier

---
+++ lib/data/jobs_repository.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'jobs_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dioHash() => r'400853981ed20563ed60f4f42cbe9ecfa38cda07';
+
+/// See also [dio].
+@ProviderFor(dio)
+final dioProvider = AutoDisposeProvider<Dio>.internal(
+  dio,
+  name: r'dioProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dioHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DioRef = AutoDisposeProviderRef<Dio>;
+String _$jobsRepositoryHash() => r'697d55ff7c33af66151c331a0ad73f030a41944e';
+
+/// See also [jobsRepository].
+@ProviderFor(jobsRepository)
+final jobsRepositoryProvider = AutoDisposeProvider<JobsRepository>.internal(
+  jobsRepository,
+  name: r'jobsRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$jobsRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef JobsRepositoryRef = AutoDisposeProviderRef<JobsRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
